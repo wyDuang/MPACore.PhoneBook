1ec8d9296dc936d63c03bf013f7d141db0f435f6
wyDuang <1014558384@qq.com>
2019-09-29 19:55:12 +0800

    项目初始化

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# MPACore.PhoneBook
-学习abp项目
+# ABP学习
---
+++ aspnet-core/src/MPACore.PhoneBook.Web.Mvc/Models/Roles/EditRoleModalViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Abp.AutoMapper;
+using MPACore.PhoneBook.Roles.Dto;
+using MPACore.PhoneBook.Web.Models.Common;
+
+namespace MPACore.PhoneBook.Web.Models.Roles
+{
+    [AutoMapFrom(typeof(GetRoleForEditOutput))]
+    public class EditRoleModalViewModel : GetRoleForEditOutput, IPermissionsEditViewModel
+    {
+        public bool HasPermission(FlatPermissionDto permission)
+        {
+            return GrantedPermissionNames.Contains(permission.Name);
+        }
+    }
+}
